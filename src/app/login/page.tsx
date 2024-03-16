c54458f9394b49cf051d4ccc7bbd77624716b619
Rasheed Johnson <rasheedjohnson@tutanota.com>
2024-03-16 16:22:09 +0200

    Added 'forgotpassword' link to app/login/page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -69,6 +69,10 @@ export default function LoginPage() {
         onClick={onLogin}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login here</button>
       <Link href="/signup">Visit Signup Page</Link>
+
+      <Link
+        className="text-blue-200"
+        href="/forgotpassword">Forgot Password</Link>
     </div>
   )
 }
